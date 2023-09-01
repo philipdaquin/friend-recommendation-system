67cdca09dda6bdf8d03f6d74a2ba0833fd309ed6
philipdaquin <philipdaquin20@gmail.com>
2023-09-01 16:36:57 +0800

    first commit

---
+++ user_service/src/test/java/com/example/user_service/TestUserServiceApplication.java
@@ -0,0 +1,30 @@
+package com.example.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestUserServiceApplication {
+
+	@Bean
+	@ServiceConnection
+	PostgreSQLContainer<?> postgresContainer() {
+		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+	}
+
+	@Bean
+	@ServiceConnection(name = "redis")
+	GenericContainer<?> redisContainer() {
+		return new GenericContainer<>(DockerImageName.parse("redis:latest")).withExposedPorts(6379);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(UserServiceApplication::main).with(TestUserServiceApplication.class).run(args);
+	}
+
+}
