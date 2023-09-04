5b9f23480b2789423572a4a855d5bd8a95a3f7b7
philipdaquin <philipdaquin20@gmail.com>
2023-09-04 12:43:57 +0800

    friend service

---
+++ friend_service/src/main/java/com/example/friend_service/FriendServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.friend_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendServiceApplication.class, args);
+	}
+
+}
---
+++ friend_service/src/test/java/com/example/friend_service/FriendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.friend_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
