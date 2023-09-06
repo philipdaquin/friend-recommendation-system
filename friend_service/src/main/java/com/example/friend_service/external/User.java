8f7a7c4fd40849681f3a6b3a0ddd9952e939e96c
philipdaquin <philipdaquin20@gmail.com>
2023-09-06 12:22:01 +0800

    fixed user entity for friend service

--- friend_service/src/main/java/com/example/friend_service/external/User.java
+++ friend_service/src/main/java/com/example/friend_service/external/User.java
@@ -1,37 +1,14 @@
 package com.example.friend_service.external;
 
-import java.io.Serializable;
-
-import org.hibernate.validator.constraints.UniqueElements;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Column;
-import org.springframework.data.relational.core.mapping.Table;
-
 import com.example.friend_service.domains.AbstractAuditingEntity;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Size;
-
-@Table(name = "users")
-public class User extends AbstractAuditingEntity implements Serializable {
+public class User extends AbstractAuditingEntity {
 
     private static final long serialVersionId = 1L;
     
-    @Id
     private Long id;
-
-    @Size(max = 50)
-    @Column(value = "first_name")
     private String firstName;
-
-    @Size(max = 50)
-    @Column(value = "last_name")
     private String lastName;
-    
-    @Email
-    @Size(min = 5, max = 254)
-    @UniqueElements
-    @Column(value = "email")
     private String email;
 
 
