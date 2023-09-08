c0716a94c6cb3eeec57b87f477d51d3dc8cb2520
philipdaquin <philipdaquin20@gmail.com>
2023-09-08 12:08:23 +0800

    bootstrap

--- discovery_service/src/test/java/com/example/discovery_service/DiscoveryServiceApplicationTests.java
+++ discovery_service/src/test/java/com/example/discovery_service/DiscoveryServiceApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.discovery_service;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class DiscoveryServiceApplicationTests {
 
+	@Autowired
+	private ApplicationContext context;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		assertNotNull(context, "The application context should be null");
 	}
 
 }
