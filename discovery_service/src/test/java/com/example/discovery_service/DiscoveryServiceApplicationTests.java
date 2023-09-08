0e1a7d5dc52ac4ddecc1fca98877c35bfe68a81a
philipdaquin <philipdaquin20@gmail.com>
2023-09-08 13:00:57 +0800

    discovery

--- discovery_service/src/test/java/com/example/discovery_service/DiscoveryServiceApplicationTests.java
+++ discovery_service/src/test/java/com/example/discovery_service/DiscoveryServiceApplicationTests.java
@@ -6,8 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DiscoveryServiceApplicationTests {
 
 	@Autowired
