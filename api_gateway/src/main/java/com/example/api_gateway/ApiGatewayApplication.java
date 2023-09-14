3b2510ca7b23ce91d26a85b2a6726bd6c0ae6bbd
philipdaquin <philipdaquin20@gmail.com>
2023-09-14 21:34:45 +0800

    sad

--- api_gateway/src/main/java/com/example/api_gateway/ApiGatewayApplication.java
+++ api_gateway/src/main/java/com/example/api_gateway/ApiGatewayApplication.java
@@ -2,20 +2,35 @@ package com.example.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 
-@SpringBootApplication
-@EnableDiscoveryClient
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+
 @EnableWebFlux
+@SpringBootApplication
+@EnableEurekaClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
+
+	@Bean
+    public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer(){
+        return factory -> factory.configureDefault(
+            id -> new Resilience4JConfigBuilder(id)
+				.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+				.build()
+        );
+    }
 
 	@Bean
 	@LoadBalanced
