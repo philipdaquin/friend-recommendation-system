4301aecb7ea7961e424a59ce2c12a9467c8b5f6f
philipdaquin <philipdaquin20@gmail.com>
2023-09-14 21:34:44 +0800

    gateway fixes

---
+++ api_gateway/src/main/java/com/config/CircuitBreakerConfiguration.java
@@ -0,0 +1,32 @@
+package com.config;
+import java.time.Duration;
+
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CircuitBreakerConfiguration {
+    private static final Duration TIMEOUT = Duration.ofSeconds(1);
+
+	@Bean
+	public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+		return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+				.circuitBreakerConfig(CircuitBreakerConfig.custom()
+						.slidingWindowSize(20)
+						.permittedNumberOfCallsInHalfOpenState(5)
+						.failureRateThreshold(50)
+						.waitDurationInOpenState(Duration.ofSeconds(60))
+						.build())
+				.timeLimiterConfig(TimeLimiterConfig.custom()
+						.timeoutDuration(TIMEOUT)
+						.build())
+				.build()
+		);
+	}
+}
