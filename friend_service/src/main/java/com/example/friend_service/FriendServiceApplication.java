1cd54ee3034edcd9d8e31da2335c95bcf7514f77
philipdaquin <philipdaquin20@gmail.com>
2023-09-17 18:23:30 +0800

    removed unnecessary tests

--- friend_service/src/main/java/com/example/friend_service/FriendServiceApplication.java
+++ friend_service/src/main/java/com/example/friend_service/FriendServiceApplication.java
@@ -3,8 +3,8 @@ package com.example.friend_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// @EnableWebFlux
 // @EnableR2dbcRepositories
+// @EnableWebFlux
 @SpringBootApplication
 public class FriendServiceApplication {
 
--- friend_service/src/test/java/com/example/friend_service/MongoContainers.java
+++ friend_service/src/test/java/com/example/friend_service/MongoContainers.java
@@ -1,5 +1,6 @@
 package com.example.friend_service;
 
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -8,6 +9,7 @@ public class MongoContainers {
 	private static final String IMAGE_NAME = "mongo:5.0";
 	private static final String IMAGE_NAME_PROPERTY = "mongo.default.image.name";
 
+	@ServiceConnection
 	public static MongoDBContainer getDefaultContainer() {
 		return new MongoDBContainer(DockerImageName.parse(System.getProperty(IMAGE_NAME_PROPERTY, IMAGE_NAME)))
 				.withReuse(true);
