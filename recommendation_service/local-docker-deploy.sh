d7218250387dc078e2dd0feec6b22c1e7d37f980
philipdaquin <philipdaquin20@gmail.com>
2023-09-29 15:50:09 +0800

    application profiles, docker compose and bash

---
+++ recommendation_service/local-docker-deploy.sh
@@ -0,0 +1,25 @@
+
+docker network create recommendation-service-local
+
+
+docker kill recommendation-service
+docker rm recommendation-service
+
+# Build 
+./mvnw clean compile 
+
+# # Test 
+./mvnw test 
+
+# Package 
+./mvnw package -Dmaven.test.skip=true
+
+# Docker build image
+docker build -t recommendation-service .
+
+docker tag recommendation-service philipasd/recommendation-service:v0.0.0
+
+docker push philipasd/recommendation-service:v0.0.0
+
+# Run the user_api container
+# docker run --name user-service -d -t --link postgres-db:postgres -p 7000:7000 user-service
