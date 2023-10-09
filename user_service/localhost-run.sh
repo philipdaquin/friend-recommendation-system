082bed9e656ff8e2547c908e3cf664c16612bc1b
philipdaquin <philipdaquin20@gmail.com>
2023-10-09 09:46:01 +0800

    working discovery service

--- user_service/localhost-run.sh
+++ user_service/localhost-run.sh
@@ -1,16 +1,14 @@
 
 docker kill postgres
 docker rm postgres
+docker kill redis 
+docker rm redis 
+
 docker pull postgres
-
-docker run -d --name postgres \                                                  
-    --rm \
-    -e POSTGRES_PASSWORD=password \
-    -e POSTGRES_USER=postgres \
-    -e POSTGRES_DB=users \
-    -p 5432:5432 \
-    postgres
-
-
 docker pull redis
+
+
+docker run -d --name postgres --rm -e POSTGRES_PASSWORD=password -e POSTGRES_USER=postgres -e POSTGRES_DB=users -p 5432:5432 postgres
+
+
 docker run --name redis -p 6379:6379 -d redis
--- user_service/src/main/java/com/example/user_service/UserServiceApplication.java
+++ user_service/src/main/java/com/example/user_service/UserServiceApplication.java
@@ -1,14 +1,17 @@
 package com.example.user_service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-// @EnableR2dbcRepositories
-// @EnableWebFlux
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class UserServiceApplication {
@@ -17,5 +20,5 @@ public class UserServiceApplication {
 
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
-
 }
+
