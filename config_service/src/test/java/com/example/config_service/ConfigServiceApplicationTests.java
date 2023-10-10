96eafe52df4e0b2f4c7e2ec3de5f75f8a4156f70
philipdaquin <philipdaquin20@gmail.com>
2023-10-10 18:11:44 +0800

    config service

---
+++ config_service/src/test/java/com/example/config_service/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.config_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
