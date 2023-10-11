58e806b62afb607ad4ce24b4cf04b17764c3f742
philipdaquin <philipdaquin20@gmail.com>
2023-10-11 17:33:58 +0800

    BREAKING: recommendation

--- recommendation_service/local-run-dependencies.sh
+++ recommendation_service/local-run-dependencies.sh
@@ -1,7 +1,7 @@
 docker kill neo4j 
 docker rm neo4j 
-docker kill kafka_broker
-docker rm kafka_broker
+docker kill kafka
+docker rm kafka
 
 docker kill zookeeper
 docker rm zookeeper
@@ -14,7 +14,16 @@ docker pull confluentinc/cp-kafka:latest
 docker run -d --name neo4j -p 7475:7474 -p 7687:7687 -p 7473:7473 -e NEO4J_PASSWORD=password -e NEO4J_HOST=username -e NEO4J_ALLOW_UPGRADE=true bitnami/neo4j:latest
 
 # Run Zookeeper
-docker run -d --name zookeeper -e ZOOKEEPER_CLIENT_PORT=2181 -e ZOOKEEPER_TICK_TIME=2000 confluentinc/cp-zookeeper:latest
-
+docker run -d \
+ 	--net=host \
+ 	--name=zookeeper \
+ 	-e ZOOKEEPER_CLIENT_PORT=32181 \
+ 	-e ZOOKEEPER_TICK_TIME=2000 \
+ 	confluentinc/cp-zookeeper:3.0.0
 # Run Kafka Broker
-docker run -d --name kafka_broker -p 9191:9191 -e KAFKA_BROKER_ID=1 -e KAFKA_ZOOKEEPER_CONNECT=zookeeper:2181 -e KAFKA_LISTENER_SECURITY_PROTOCOL_MAP=PLAINTEXT:PLAINTEXT -e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://kafka_broker:9191 -e KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR=1 -e KAFKA_TRANSACTION_STATE_LOG_MIN_ISR=1 -e KAFKA_TRANSACTION_STATE_LOG_REPLICATION_FACTOR=1 confluentinc/cp-kafka:latest
+ docker run -d \
+ 	--net=host \
+ 	--name=kafka \
+ 	-e KAFKA_ZOOKEEPER_CONNECT=localhost:32181 \
+ 	-e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://localhost:9191 \
+ 	confluentinc/cp-kafka:3.0.0
