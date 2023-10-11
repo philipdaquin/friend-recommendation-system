38f92b679ca4dc192e5b52e5a7345c5c65949e82
philipdaquin <philipdaquin20@gmail.com>
2023-10-11 13:17:16 +0800

    BREAKING: Friend service

--- friend_service/localhost-run.sh
+++ friend_service/localhost-run.sh
@@ -2,4 +2,7 @@
 docker kill mongo-container 
 docker rm mongo-container
 
+
+docker pull mongo:latest
+
 docker run -d --name mongo-container --restart=always -e MONGO_INITDB_ROOT_USERNAME=username -e MONGO_INITDB_ROOT_PASSWORD=password -p 27017:27017 -v mongodb_data_container:/data/db mongo:latest
--- friend_service/src/main/java/com/example/friend_service/FriendServiceApplication.java
+++ friend_service/src/main/java/com/example/friend_service/FriendServiceApplication.java
@@ -2,11 +2,12 @@ package com.example.friend_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 // @EnableR2dbcRepositories
 // @EnableWebFlux
 @SpringBootApplication
-
+@EnableDiscoveryClient
 public class FriendServiceApplication {
 
 	public static void main(String[] args) {
