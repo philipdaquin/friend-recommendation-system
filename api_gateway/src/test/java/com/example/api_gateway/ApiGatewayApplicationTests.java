e16b69c65fedfaf32c15ef5a56846cc24b4b754f
philipdaquin <philipdaquin20@gmail.com>
2023-10-11 10:41:53 +0800

    BREAKING: removed config director deps

--- api_gateway/src/test/java/com/example/api_gateway/ApiGatewayApplicationTests.java
+++ api_gateway/src/test/java/com/example/api_gateway/ApiGatewayApplicationTests.java
@@ -3,11 +3,11 @@ package com.example.api_gateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class ApiGatewayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
